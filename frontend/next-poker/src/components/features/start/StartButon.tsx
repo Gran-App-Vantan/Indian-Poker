6e8c8684cd961ec7d8c7fb68ab2c354c6dc44f4e
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-10-03 15:45:10 +0900

    Adding hover to StartButton

--- frontend/next-poker/src/components/features/start/StartButon.tsx
+++ frontend/next-poker/src/components/features/start/StartButon.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export function StartButton() {
     return (
-        <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-10 hover:cursor-pointer">
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10 hover:cursor-pointer hover:scale-125 transition-transform duration-500">
             <Image
                 src="/start/StartButton.png"
                 alt="start-button"
