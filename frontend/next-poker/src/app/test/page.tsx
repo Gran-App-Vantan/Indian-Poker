f0b8dc0d19c16dded51b112f7b9dffe82e35dda0
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-10-22 01:47:28 +0900

    Completing Payment Settings

--- frontend/next-poker/src/app/test/page.tsx
+++ frontend/next-poker/src/app/test/page.tsx
@@ -1,23 +1,17 @@
 "use client";
 import { useState } from "react";
 import { Modal } from "@/components/shared/Modal";
-import{LoginButton} from"@/components/features/start/LoginButton";
 import {Qr} from"@/components/features/start/Qr";
 import {OperationInstructions} from"@/components/features/start/OperationInstructions";
 import { Stanby } from "@/components/features/start/Standby";
+import { PaymentSettings } from "@/components/features/game/PaymentSettings";
 
 export default function Test() {
     const [showModal, setShowModal] = useState(false);
 
     return (
         <div>
-            <Modal
-                size="large"
-                openModal={showModal}
-                onClose={() => setShowModal(false)}
-                >
-                    <Stanby />
-            </Modal>
+            <PaymentSettings />
         </div>
     );
 }
