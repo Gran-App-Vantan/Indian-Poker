2c19a5b8d9aa345044a3eb72f995fafa7f58a1b2
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-11-02 00:45:30 +0900

    createTimerComponet Appearance

---
+++ frontend/next-poker/src/components/features/game/Timer.tsx
@@ -0,0 +1,11 @@
+
+
+export function Timer() {
+    return (
+        <div className="relative top-5 left-14 w-[191] h-22 bg-[linear-gradient(135deg,#7A5C2E_0%,#C59D4D_35%,#FFF3B0_55%,#C59D4D_80%,#7A5C2E_100%)] rounded-3xl p-2">
+            <div className="flex items-center justify-center bg-linear-to-r from-[#000000] via-[#290106] to-[#000000] text-white rounded-2xl w-full h-full text-6xl">
+                <p>00:00</p>
+            </div>
+        </div>
+    );
+}
