58ae595d84be4657fcc5b4efd246109e84fcd868
junpei-chan <jumpei24s1102270@gmail.com>
2025-11-10 14:18:23 +0900

    add: index.ts

---
+++ frontend/next-poker/src/components/features/start/index.ts
@@ -0,0 +1,6 @@
+export * from "./LoginModalContent";
+export * from "./Logo";
+export * from "./OperationInstructions";
+export * from "./Qr";
+export * from "./Standby";
+export * from "./StartButon";
