fa222f26864c749c19e8b8d510fea50ebb40c8b0
junpei-chan <jumpei24s1102270@gmail.com>
2025-11-12 01:08:45 +0900

    add: getPlayingUsers.ts

---
+++ frontend/next-poker/src/api/game/getPlayingUsers.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import humps from "humps";
+
+type GetPlayingUsersResponse = 
+  | {
+    success: true;
+    message: string;
+    users: {
+      id: number;
+      snsId: number;
+      name: string;
+      userIcon: string;
+    }
+  }
+  | {
+    success: false;
+    message: string;
+  }
+
+export async function GetPlayingUsers():Promise<GetPlayingUsersResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_GAME_API_URL}/game/is-playing-user`;
+  const authToken = Cookies.get("authToken");
+
+  return axios
+    .get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        Accept: "application/json"
+      },
+    })
+    .then((res) => {
+      res.data = humps.camelizeKeys(res.data) as typeof res.data;
+      return res.data;
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
