967be42a69627032c83818010669a8feaee13f11
junpei-chan <jumpei24s1102270@gmail.com>
2025-11-15 01:48:04 +0900

    add: Player type

--- frontend/next-poker/src/api/game/types.ts
+++ frontend/next-poker/src/api/game/types.ts
@@ -4,3 +4,11 @@ export interface Card {
   type: "heart" | "diamond" | "spade" | "club" | "jokerRed" | "jokerBlack";
   imagePath: string;
 }
+
+export interface Player {
+  id: number;
+  card: "heart" | "diamond" | "spade" | "club" | "jokerRed" | "jokerBlack";
+  latch: number;
+  point: number;
+  isSet: number; // 0と1で判断
+};
