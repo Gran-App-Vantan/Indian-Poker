f43011c86462df7a85ef814736adfa5c7d721f41
junpei-chan <jumpei24s1102270@gmail.com>
2025-11-14 16:11:12 +0900

    add: currentOptions.ts

---
+++ frontend/next-poker/src/api/game/currentOptions.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import humps from "humps";
+import { Card } from "./types";
+
+export interface CurrentOptionsResponse {
+  cardOffer: [
+    Card[]
+  ]
+};
+
+export async function CurrentOptions(): Promise<CurrentOptionsResponse> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_GAME_URL}/game/current-options`;
+  const authToken = Cookies.get("authToken");
+
+  return axios
+    .get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        Accept: "application/json"
+      },
+    })
+    .then((res) => {
+      res.data = humps.camelizeKeys(res.data) as typeof res.data;
+      return res.data;
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
